0c616ddd0aeca98a24b1bb384116a334aed229c0
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-10 16:27:22 +0200

    .

--- src/main/java/information/DateManager.java
+++ src/main/java/information/DateManager.java
@@ -1,4 +1,23 @@
 package information;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class DateManager {
+    private List<LocalDate> dates;
+
+    public DateManager() {
+        this.dates = new ArrayList<>();
+    }
+
+    public void addDate(LocalDate date) {
+        this.dates.add(date);
+        Collections.sort(this.dates);
+    }
+
+    public List<LocalDate> getDates() {
+        return this.dates;
+    }
 }
