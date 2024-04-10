08e8dd8c3708b9935ded71efeae666da04316cdd
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-10 16:29:26 +0200

    .

--- src/main/java/numeric/NumberListing.java
+++ src/main/java/numeric/NumberListing.java
@@ -1,4 +1,10 @@
 package numeric;
 
 public class NumberListing {
+
+    public void listNumbers(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            System.out.println(i);
+        }
+    }
 }
