693600715bbcd37c4cae741c0158a452aac5880e
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-10 16:29:09 +0200

    .

--- src/main/java/numeric/MaxFinder.java
+++ src/main/java/numeric/MaxFinder.java
@@ -1,4 +1,14 @@
 package numeric;
 
 public class MaxFinder {
+
+    public int findMax(int[] numbers) {
+        int max = numbers[0];
+        for (int number : numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
+    }
 }
