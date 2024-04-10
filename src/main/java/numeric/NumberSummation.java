ac75ce8ddf028adc9d9569de030d1ce4ba33e1c9
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-10 16:29:44 +0200

    .

--- src/main/java/numeric/NumberSummation.java
+++ src/main/java/numeric/NumberSummation.java
@@ -1,4 +1,12 @@
 package numeric;
 
 public class NumberSummation {
+
+    public int sumNumbers(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return n + sumNumbers(n - 1);
+        }
+    }
 }
