957517eda8e4f9bc866c2eac433507d5cf4edd30
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-10 16:26:38 +0200

    .

--- src/main/java/analysis/GeneCounter.java
+++ src/main/java/analysis/GeneCounter.java
@@ -1,4 +1,21 @@
 package analysis;
 
 public class GeneCounter {
+
+    public int countGenes(String dnaSequence) {
+        return countGenesHelper(dnaSequence, 0);
+    }
+
+    private int countGenesHelper(String dnaSequence, int index) {
+        if (index >= dnaSequence.length() - 2) {
+            return 0;
+        }
+
+        String codon = dnaSequence.substring(index, index + 3);
+        if (codon.equals("ATG")) {
+            return 1 + countGenesHelper(dnaSequence, index + 3);
+        } else {
+            return countGenesHelper(dnaSequence, index + 1);
+        }
+    }
 }
