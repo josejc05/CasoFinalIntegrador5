070bf8fd2e6f26f98ac827fe2d712ec8dec9169e
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-10 16:26:56 +0200

    .

--- src/main/java/analysis/GeneticCombinations.java
+++ src/main/java/analysis/GeneticCombinations.java
@@ -1,4 +1,12 @@
 package analysis;
 
 public class GeneticCombinations {
+
+    public int calculateCombinations(int geneLength) {
+        if (geneLength <= 1) {
+            return 1;
+        } else {
+            return geneLength * calculateCombinations(geneLength - 1);
+        }
+    }
 }
